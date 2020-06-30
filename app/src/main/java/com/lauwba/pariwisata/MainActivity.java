ba073fe39cf2c8fcad32278df92a8b49be63d589
Zelvia Olga Maharani <zelviaolgamaharani98@gmail.com>
2020-06-30 02:30:45 +0100

    Initial commit

---
+++ app/src/main/java/com/lauwba/pariwisata/MainActivity.java
@@ -0,0 +1,131 @@
+package com.lauwba.pariwisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ViewListener;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    CarouselView customCarouselView;
+    int[] sampleImages = {R.drawable.ntt1, R.drawable.ntt2, R.drawable.ntt3, R.drawable.ntt4, R.drawable.ntt5};
+    String[] sampleTitles = {"Orange", "Grapes", "Strawberry", "Cherry", "Apricot"};
+
+    TextView tvToday;
+    String hariIni;
+
+//    CardView home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        customCarouselView = (CarouselView) findViewById(R.id.customCarouselView);
+        customCarouselView.setPageCount(sampleImages.length);
+        customCarouselView.setSlideInterval(4000);
+        customCarouselView.setViewListener(viewListener);
+
+        tvToday = findViewById(R.id.tvDate);
+        //get Time Now
+        Date dateNow = Calendar.getInstance().getTime();
+        hariIni = (String) DateFormat.format("EEEE", dateNow);
+        getToday();
+
+        CardView home = (CardView) findViewById(R.id.home);
+        CardView portal = (CardView) findViewById(R.id.portalwisata);
+        CardView lokasi = (CardView) findViewById(R.id.lokasiwisata);
+        CardView video = (CardView) findViewById(R.id.videowisata);
+        CardView galeri = (CardView) findViewById(R.id.galeriwisata);
+        CardView tentang = (CardView) findViewById(R.id.tentangwisata);
+        home.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,BerandaActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        portal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,PortalActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        lokasi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,BerandaActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        video.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,BerandaActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        galeri.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,BerandaActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        tentang.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,TentangActivity.class);
+//                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+    private void getToday() {
+        Date date = Calendar.getInstance().getTime();
+        String tanggal = (String) DateFormat.format("d MMMM yyyy", date);
+        String formatFix = hariIni + ", " + tanggal;
+        tvToday.setText(formatFix);
+    }
+
+
+//    // To set custom views
+    ViewListener viewListener = new ViewListener() {
+        @Override
+        public View setViewForPosition(int position) {
+
+            View customView = getLayoutInflater().inflate(R.layout.header, null);
+            TextView labelTextView = (TextView) customView.findViewById(R.id.labelTextView);
+            ImageView fruitImageView = (ImageView) customView.findViewById(R.id.fruitImageView);
+            fruitImageView.setImageResource(sampleImages[position]);
+            labelTextView.setText(sampleTitles[position]);
+            return customView;
+        }
+    };
+
+}
---
+++ app/src/main/java/com/lauwba/pariwisata/PortalActivity.java
@@ -0,0 +1,82 @@
+package com.lauwba.pariwisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class PortalActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+    WebSettings webSettings;
+
+    String URL = "http://www.lauwba.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portal);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        webView = (WebView) findViewById(R.id.web_view);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Untuk mengaktifkan javascript
+        webSettings.getUseWideViewPort();
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                // Menampilkan loading ketika webview proses load halaman
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient(){
+            // Ketika webview error atau selesai load page loading akan dismiss
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.loadUrl(URL);
+    }
+
+    @Override
+    public void onRefresh() {
+        // Untuk refresh webview dengan swipe
+        webView.reload();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Jika Webview bisa di back maka backward page sebelumnya
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+}
